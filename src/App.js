373d6711f2880d0872c763a77356a59b3d026dc4
shivaay108 <shivaay4339@gmail.com>
2023-03-08 23:36:55 +0530

    added all the files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {useState} from 'react';
 function App() {
+
+  const [count,changeCount] = useState(100) // hook which takes array such as it will
+  // hold the state
+
+  const gainCount = () => {
+    changeCount(count+1);
+  }
+  const decreaseCount = () => {
+    changeCount(count-1);
+  }
+    
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="App"> 
+        <h1>{count}</h1>
+        <button onClick={gainCount}>Change count</button>
+        <button onClick={decreaseCount}>Decrease it!!</button>
     </div>
   );
 }
